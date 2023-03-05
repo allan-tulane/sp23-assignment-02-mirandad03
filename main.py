3fa82aa89f2b78844540e142ac7296f334913613
Miranda <93054741+mirandad03@users.noreply.github.com>
2023-03-05 02:36:20 +0000

    Written Answers pdf

--- main.py
+++ main.py
@@ -12,6 +12,12 @@ class BinaryNumber:
         
     def __repr__(self):
         return('decimal=%d binary=%s' % (self.decimal_val, ''.join(self.binary_vec)))
+
+    def __add__(self, other):
+        return BinaryNumber(self.decimal_val + other.decimal_val)
+
+    def __sub__(self, other):
+        return BinaryNumber(self.decimal_val - other.decimal_val)
     
 
 ## Implement multiplication functions here. Note that you will have to
@@ -43,22 +49,50 @@ def pad(x,y):
         y = ['0'] + y
     return x,y
 
+def subquadratic_multiply(x_bin, y_bin):
+    
+    # pad binary vectors to ensure even length
+    x_bin, y_bin = pad(x_bin, y_bin)
+    
+    # base case: single bit multiplication
+    if len(x_bin) == 2:
+        return BinaryNumber(int(x_bin[0]) * int(y_bin[0]))
+    
+    # split input binary vectors in half
+    a, b = split_number(x_bin)
+    c, d = split_number(y_bin)
+    
+    # compute products of the halves
+    ac = subquadratic_multiply(a, c)
+    bd = subquadratic_multiply(b, d)
+    
+    # compute product of (a+b) and (c+d) using Karatsuba-Ofman recursion
+    ab = binary2int(a + b)
+    cd = binary2int(c + d)
+    ab_cd = subquadratic_multiply(ab.binary_vec, cd.binary_vec)
+    ad_bc = ab_cd - ac - bd
+    
+    # combine products using Karatsuba-Ofman formula
+    return ac + bit_shift(BinaryNumber(ad_bc), len(x_bin)//2) + bit_shift(bd, len(x_bin))
 
-
-def subquadratic_multiply(x, y):
-    ### TODO
-    pass
-    ###
-
-## Feel free to add your own tests here.
-def test_multiply():
-    assert subquadratic_multiply(BinaryNumber(2), BinaryNumber(2)) == 2*2
+def multiply(a, b):
+    # Convert both values to binary
+    bin_a = Binary(a)
+    bin_b = Binary(b)
+    
+    # Multiply the binary values and convert the result to decimal
+    result = Binary(bin_a.binary_val * bin_b.binary_val, decimal=False)
+    
+    return result
 
 def time_multiply(x, y, f):
     start = time.time()
-    # multiply two numbers x, y using function f
-    return (time.time() - start)*1000
+    result = f(x.binary_vec, y.binary_vec)
+    timetaken = (time.time() - start)*1000
+    print("Time:", timetaken)
+    return timetaken
 
-    
-    
-
+test_multiply()
+x = BinaryNumber(1234)
+y = BinaryNumber(5678)
+time_multiply(x, y, subquadratic_multiply)
